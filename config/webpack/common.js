69266b0e857507c43e07fc0d2c56274fa1634e4d
Michal Komorowski <mkomorowski@windowslive.com>
2021-09-18 00:33:47 +0100

    Upgrade typescript version

--- config/webpack/common.js
+++ config/webpack/common.js
@@ -17,7 +17,7 @@ module.exports = {
       },
       {
         test: /\.tsx?$/,
-        use: ["babel-loader", "awesome-typescript-loader"],
+        use: ["babel-loader"],
       },
       {
         test: /\.css$/,
